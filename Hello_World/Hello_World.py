3972080c8003e75ed4c70644174988535fee0df2
Kris Lange <lange649@umn.edu>
2015-07-29 19:27:53 -0500

    Hello World init commit

---
+++ Hello_World/Hello_World.py
@@ -0,0 +1,13 @@
+################################################################################
+##                                                                            ##
+##   Title:     Hello_World.py                                                ##
+##   Author:    Kris Lange                                                    ##
+##                                                                            ##
+##   Description: Introductory Hello World Program in Python3                 ##
+##                    Prints "Hello World" on the console                     ##
+##                    For more information, type help(print)                  ##
+##                    into the console                                        ##
+##                                                                            ##
+################################################################################
+
+print("Hello World")
