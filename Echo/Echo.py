49c1e646c5b0231a9e1095308208f66277e123be
Kris Lange <lange649@umn.edu>
2015-08-04 20:26:04 -0500

    added Echo demo

---
+++ Echo/Echo.py
@@ -0,0 +1,15 @@
+################################################################################
+##                                                                            ##
+##   Title:     Echo.py                                                       ##
+##   Author:    Kris Lange                                                    ##
+##                                                                            ##
+##   Description: A program which will echo what the user types               ##
+##                    Teaches the programmer to read in user input            ##
+##                                                                            ##
+################################################################################
+
+# Prompts the user to enter something, saves response in foo
+foo = input("")
+
+# Echoes user's response which was saved in foo
+print(foo)
